4df2a9118a9e8cefcebd8d3e6a66d7fb5b79dae9
gfbatista <gilbertoferrari@gmail.com>
2024-07-31 09:24:43 -0300

    chore: initial commit

---
+++ src/main/kotlin/com/delivery/delivery_worker/DeliveryWorkerApplication.kt
@@ -0,0 +1,11 @@
+package com.delivery.delivery_worker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryWorkerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeliveryWorkerApplication>(*args)
+}
---
+++ src/test/kotlin/com/delivery/delivery_worker/DeliveryWorkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.delivery.delivery_worker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DeliveryWorkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
